23d606cc64cad6998b30f19a7c827b0795ae644b
Patrick Kolodziej <koloooo91@googlemail.com>
2017-10-11 22:05:37 +0200

    first commit

---
+++ app/src/main/java/thekolo/de/widgetsforikeatradfri/MainActivity.kt
@@ -0,0 +1,24 @@
+package thekolo.de.widgetsforikeatradfri
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //65540 Stehlampe
+        val ip = "192.168.178.56"
+        val securityId = "vBPnZjwbl07N8rex"
+        val stehlampe = "65540"
+
+        val client = TradfriClient(ip, securityId)
+        val device = client.getDevice(stehlampe)
+        val turnOffResponse = client.turnDeviceOn(stehlampe)
+        val response = client.getDevices()
+        println(response)
+
+    }
+}
