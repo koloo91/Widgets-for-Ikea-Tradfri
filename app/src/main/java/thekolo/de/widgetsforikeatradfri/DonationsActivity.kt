f49203c6b38e67b86249df83079d010d6cd3e055
Patrick Kolodziej <koloooo91@googlemail.com>
2018-02-11 11:27:15 +0100

    Use correct pub key

--- app/src/main/java/thekolo/de/widgetsforikeatradfri/DonationsActivity.kt
+++ app/src/main/java/thekolo/de/widgetsforikeatradfri/DonationsActivity.kt
@@ -40,7 +40,7 @@ class DonationsActivity : AppCompatActivity() {
     }
 
     companion object {
-        private val GOOGLE_PUBKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAg8bTVFK5zIg4FGYkHKKQ/j/iGZQlXU0qkAv2BA6epOX1ihbMz78iD4SmViJlECHN8bKMHxouRNd9pkmQKxwEBHg5/xDC/PHmSCXFx/gcY/xa4etA1CSfXjcsS9i94n+j0gGYUg69rNkp+p/09nO9sgfRTAQppTxtgKaXwpfKe1A8oqmDUfOnPzsEAG6ogQL6Svo6ynYLVKIvRPPhXkq+fp6sJ5YVT5Hr356yCXlM++G56Pk8Z+tPzNjjvGSSs/MsYtgFaqhPCsnKhb55xHkc8GJ9haq8k3PSqwMSeJHnGiDq5lzdmsjdmGkWdQq2jIhKlhMZMm5VQWn0T59+xjjIIwIDAQAB"
+        private val GOOGLE_PUBKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiCym60cI3MrIUpXiIcTudnFTOwc8c53CadvIQQkDAXeExysKiRF1rUvp567k7bAJKEuN6E1EuE4BW5fyKi2ydo0NrVGxgXj2tW4hh9Z1VWMh6nIeUJtJGxFI0/9n50QGEzUjOSQvaN/3T+7vGH0HBA0mOZ37QO0RZa08srK+VTWvzFNLilgM3/VeSEGfsc3zHJgm+HU+SiBSxl4M1HbgX0+T/qdBJvFYHWCXVC8GWKdaq/RI3k7l9+2h1Htq1vrGmRQKOzW7xo0urNCxCktfPlByWYMMKya1wX7FSuzQmvwR7fbxCvwcDhleTwFhvzRBKnXn+MQExse37P+XHuzGvwIDAQAB"
         private val GOOGLE_CATALOG = arrayOf("ntpsync.donation.1", "ntpsync.donation.2", "ntpsync.donation.3", "ntpsync.donation.5", "ntpsync.donation.8", "ntpsync.donation.13")
     }
 }
